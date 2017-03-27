e2919efa4d5cd3518eca4f614a9bd057511fcc05
fblanco <fblanco@FBlanco.local>
2017-03-27 16:36:16 -0600

    Gestion de empleados v1

--- app/assets/javascripts/indexAdministrador.js
+++ app/assets/javascripts/indexAdministrador.js
@@ -1,7 +1,7 @@
 function cambiarEstado(id_usuario, estado){
 	cambia = false;
 	swal({
-		title: "¿Confirmar cambio de estado de este usuario?" + estado.value,
+		title: "¿Confirmar cambio de estado de este usuario?" + estado,
 		text: "Esto afectara el ingreso del usuario al sistema",
 		type: "warning",
 		showCancelButton: true,
@@ -17,14 +17,70 @@ function cambiarEstado(id_usuario, estado){
 	   				url: '/usuario/',
 	   				method: 'PUT',
 	   				data: {
-	   					id_usuario: id_usuario
+	   					id_usuario: id_usuario,
+	   					estado: estado
 	   				},
-	   				success: function(res){
-	   					swal('Estado cambiado!', "...", "success");
+	   				success: function(xml, textStatus, xhr){
+	   					if(xhr.status == 200)
+	   						swal('Estado cambiado!', "...", "success");
+	   					else
+	   						swal('Error al cambiar al estado', "...", "error");
 	   				}
 	   			})
-	   		}
-	   }
+	   		}else{
+	   			return false;
+	   		}   
+	   	}
 	)
-	return cambia;
+	return false;
 }
+
+function buscarUsuario(form){
+	usuario = form.nombre.value || "";
+	if(usuario != ""){
+		$.get('/../api/usuario/'+usuario, function(data, status){
+			usuario = `
+					<tr class="row">
+						<td>
+							${data.nombre}
+						</td>
+						<td>
+							${data.rol}
+						</td>
+						<td>
+							<div class="input-field">
+								<select  name="estado" onchange="return cambiarEstado(${data.id_usuario}, '${data.estado}');" >
+									`;
+									if(data.estado == "activo"){
+										usuario += `
+													<option value="activo" selected="true"> Activo </option>
+													<option value="inactivo" > Inactivo </option>
+													`;
+									}else if(data.estado == "inactivo"){
+										usuario += `
+													<option value="activo"> Activo </option>
+													<option value="inactivo" selected="true"> Inactivo </option>
+													`;
+									}
+									
+								usuario += `</select>
+							</div>
+						</td>
+						<td>
+							<a href="/empleado/${data.id_empleado}">
+							 Ver empleado </a>
+						</td>
+					</tr>
+					`;
+				$('#body_usuarios').html(usuario);
+				$(document).ready(function (){
+					$('select').material_select();
+				});
+		});
+
+	}else{
+		window.location('/');
+	}
+	return false;
+}
+
--- app/routes.js
+++ app/routes.js
@@ -80,8 +80,16 @@ module.exports = function(app, passport, express) {
         });
     // cambiar de estado    
     router.put('/usuario/', isLoggedIn, isAdministrador, function(req, res){
-        
-        res.send('si entra =>'+req.body.id_usuario);
+        id_usuario = req.body.id_usuario;
+        estado = req.body.estado;
+        nuevo_estado = (estado == 'activo') ? 'inactivo' : 'activo';
+        db.query('update usuario set estado = ? where id_usuario = ?', [nuevo_estado, id_usuario], function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.status(200).send('#');
+            }
+        });
     });
 
     // router.route('/empleado/:id')
@@ -120,6 +128,14 @@ module.exports = function(app, passport, express) {
             db.query('select *from empleado_info where num_empleado=?;',[req.params.num_empleado], function(err, rows){
                 var empleado = JSON.parse(JSON.stringify(rows));
                 res.status(200).json(empleado[0]);
+            });
+        });
+
+    api.route('/usuario/:nombre/')
+        .get(isLoggedIn, isAdministrador, function(req, res){
+            db.query("select usuario.id_usuario, usuario.nombre, usuario.estado, usuario.id_empleado,rol.id_rol, rol.nombre as rol from usuario join cat_rol as rol on usuario.id_rol = rol.id_rol where rol.nombre != 'administrador' and usuario.nombre=? limit 20;", [req.params.nombre], function(err, rows){
+                var usuario = JSON.parse(JSON.stringify(rows));
+                res.status(200).json(usuario[0]);
             });
         });
 
